727f2d94d38387f933b7ef13c710e4a08c0260a4
Simran Fathima <frahim5@gmail.com>
2025-05-28 17:23:58 +0600

    update

--- includ/backlistShortcode.php
+++ includ/backlistShortcode.php
@@ -1,18 +1,19 @@
 <?php
 add_shortcode('blacklist_checker', 'render_blacklist_checker');
 
-function render_blacklist_checker() {
+function render_blacklist_checker()
+{
     ob_start();
-    ?>
+?>
     <form method="post">
-       
+
         <div class="search">
-           <input type="text" name="blc_input" class="searchTerm" placeholder="Enter IP address or domain" required>
-        <button type="submit" class="searchButton">Check Blacklists</button>
+            <input type="text" name="blc_input" class="searchTerm" placeholder="Enter IP address or domain" required>
+            <button type="submit" class="searchButton">Check Blacklists</button>
         </div>
-        
+
     </form>
-    <?php
+<?php
 
     if (isset($_POST['blc_input'])) {
         $input = trim(sanitize_text_field($_POST['blc_input']));
@@ -22,7 +23,7 @@ function render_blacklist_checker() {
             echo "<p style='color:red;'>Invalid IP or domain.</p>";
         } else {
             $results = check_dns_blacklists($ip);
-echo "<div  class='resultwrwpper' style='margin-top: 20px;'><div class='innerwrapper'>";
+            echo "<div  class='resultwrwpper' style='margin-top: 20px;'><div class='innerwrapper'>";
             echo "<h3>Blacklist Report for <em>" . esc_html($input) . "</em> (IP: $ip)</h3>";
             echo "<table style='width:100%; border-collapse: collapse;' border='1'>";
             echo "<thead><tr><th>Blacklist</th><th>Status</th></tr></thead><tbody>";
@@ -40,47 +41,48 @@ echo "<div  class='resultwrwpper' style='margin-top: 20px;'><div class='innerwra
     return ob_get_clean();
 }
 
-function check_dns_blacklists($ip) {
-   $blacklists = [
-    "all.s5h.net",
-    "b.barracudacentral.org",
-    "bl.0spam.org",
-    "bl.spamcop.net",
-    "dnsbl-1.uceprotect.net",
-    "dnsbl-2.uceprotect.net",
-    "dnsbl-3.uceprotect.net",
-    "dyna.spamrats.com",
-    "ips.backscatterer.org",
-    "korea.services.net",
-    "noptr.spamrats.com",
-    "blacklist.woody.ch",
-    "bogons.cymru.com",
-    "combined.abuse.ch",
-    "db.wpbl.info",
-    "dnsbl.dronebl.org",
-    "drone.abuse.ch",
-    "duinv.aupads.org",
-    "orvedb.aupads.org",
-    "proxy.bl.gweep.ca",
-    "psbl.surriel.com",
-    "rbl.0spam.org",
-    "relays.bl.gweep.ca",
-    "relays.nether.net",
-    "singular.ttk.pte.hu",
-    "spam.abuse.ch",
-    "spam.dnsbl.anonmails.de",
-    "spam.spamrats.com",
-    "spambot.bls.digibase.ca",
-    "spamrbl.imp.ch",
-    "spamsources.fabel.dk",
-    "ubl.lashback.com",
-    "ubl.unsubscore.com",
-    "virus.rbl.jp",
-    "wormrbl.imp.ch",
-    "z.mailspike.net",
-    "zen.spamhaus.org",
-    "dnsbl.sorbs.net",
-];
+function check_dns_blacklists($ip)
+{
+    $blacklists = [
+        "all.s5h.net",
+        "b.barracudacentral.org",
+        "bl.0spam.org",
+        "bl.spamcop.net",
+        "dnsbl-1.uceprotect.net",
+        "dnsbl-2.uceprotect.net",
+        "dnsbl-3.uceprotect.net",
+        "dyna.spamrats.com",
+        "ips.backscatterer.org",
+        "korea.services.net",
+        "noptr.spamrats.com",
+        "blacklist.woody.ch",
+        "bogons.cymru.com",
+        "combined.abuse.ch",
+        "db.wpbl.info",
+        "dnsbl.dronebl.org",
+        "drone.abuse.ch",
+        "duinv.aupads.org",
+        "orvedb.aupads.org",
+        "proxy.bl.gweep.ca",
+        "psbl.surriel.com",
+        "rbl.0spam.org",
+        "relays.bl.gweep.ca",
+        "relays.nether.net",
+        "singular.ttk.pte.hu",
+        "spam.abuse.ch",
+        "spam.dnsbl.anonmails.de",
+        "spam.spamrats.com",
+        "spambot.bls.digibase.ca",
+        "spamrbl.imp.ch",
+        "spamsources.fabel.dk",
+        "ubl.lashback.com",
+        "ubl.unsubscore.com",
+        "virus.rbl.jp",
+        "wormrbl.imp.ch",
+        "z.mailspike.net",
+        "zen.spamhaus.org",
+        "dnsbl.sorbs.net",
+    ];
 
     $reverse_ip = implode(".", array_reverse(explode(".", $ip)));
     $results = [];
